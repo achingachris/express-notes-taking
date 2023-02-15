a4096baf728d71fa5ad0a2c2f7029534a7e5dc44
achingachris <achinga.chris@gmail.com>
2023-02-16 01:14:07 +0300

    cargo list page

--- src/pages/client/cargo/index.js
+++ src/pages/client/cargo/index.js
@@ -1,7 +1,12 @@
 import Client from '@/layout/client/Client'
+import CargoList from '@/components/Client/cargo/CargoList'
 
 const index = () => {
-  return <Client>Cargo</Client>
+  return (
+    <Client>
+      <CargoList />
+    </Client>
+  )
 }
 
 export default index
