a036cd909a5589bae2a9f2faef0aa83816b085a1
achingachris <achinga.chris@gmail.com>
2023-02-16 03:03:17 +0300

    registration pages

--- src/pages/auth/client/register.js
+++ src/pages/auth/client/register.js
@@ -4,80 +4,76 @@ const register = () => {
   return (
     <TransporterAuth>
       <div className='row'>
-        <h2 className='text-center'>Transporter Registration</h2>
-        <p className='text-center'>
-          Enter details to register as a transporter
-        </p>
+        <h2 className='text-center'>Client Registration</h2>
+        <p className='text-center'>Enter details to register as a Client</p>
         <div className='col-sm-12'>
           <div className='card'>
             <div className='card-body'>
               <form>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='companyName'>Company Name</label>
-                  <input
-                    type='text'
-                    className='form-control'
-                    id='companyName'
-                    name='companyName'
-                  />
-                </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='fullName'>Full Name of the Director</label>
+                <div className='form-group'>
+                  <label htmlFor='fullName'>
+                    Full name of Managing Director
+                  </label>
                   <input
                     type='text'
                     className='form-control'
                     id='fullName'
                     name='fullName'
+                    required=''
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='email'>Email Address</label>
+                <div className='form-group'>
+                  <label htmlFor='email'>Email address</label>
                   <input
                     type='email'
                     className='form-control'
                     id='email'
                     name='email'
+                    required=''
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='phoneNumber'>Phone Number</label>
+                <div className='form-group'>
+                  <label htmlFor='phone'>Phone number</label>
                   <input
                     type='tel'
                     className='form-control'
-                    id='phoneNumber'
-                    name='phoneNumber'
+                    id='phone'
+                    name='phone'
+                    required=''
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='companyCertificate'>
-                    Company Registration Certificate
-                  </label>
+                <div className='form-group'>
+                  <label htmlFor='companyName'>Company name</label>
                   <input
-                    type='file'
-                    className='form-control-file'
-                    id='companyCertificate'
-                    name='companyCertificate'
+                    type='text'
+                    className='form-control'
+                    id='companyName'
+                    name='companyName'
+                    required=''
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='tinNumber'>Company TIN Number</label>
+                <div className='form-group'>
+                  <label htmlFor='tinNumber'>TIN number</label>
                   <input
                     type='text'
                     className='form-control'
                     id='tinNumber'
                     name='tinNumber'
+                    required=''
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='numTrucks'>Number of Trucks</label>
-                  <input
-                    type='number'
+                <div className='form-group'>
+                  <label htmlFor='physicalAddress'>Physical Address</label>
+                  <textarea
                     className='form-control'
-                    id='numTrucks'
-                    name='numTrucks'
+                    id='physicalAddress'
+                    name='physicalAddress'
+                    rows={3}
+                    required=''
+                    defaultValue={''}
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
+                <div className='form-group'>
                   <label htmlFor='businessSpecialization'>
                     Business Specialization (Local, Transit or Both)
                   </label>
@@ -85,33 +81,39 @@ const register = () => {
                     className='form-control'
                     id='businessSpecialization'
                     name='businessSpecialization'
+                    required=''
                   >
+                    <option value=''>Please select an option</option>
                     <option value='local'>Local</option>
                     <option value='transit'>Transit</option>
                     <option value='both'>Both</option>
                   </select>
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='preferableRoutes'>Preferable Routes</label>
-                  <textarea
+                <div className='form-group'>
+                  <label htmlFor='estimateVolume'>
+                    Estimate volume per month
+                  </label>
+                  <input
+                    type='text'
                     className='form-control'
-                    id='preferableRoutes'
-                    name='preferableRoutes'
-                    defaultValue={''}
+                    id='estimateVolume'
+                    name='estimateVolume'
+                    required=''
                   />
                 </div>
-                <div className='form-group mt-3 mb-3'>
-                  <label htmlFor='relevantDocs'>
-                    Uploading of Relevant Documents
+                <div className='form-group'>
+                  <label htmlFor='relevantDocuments'>
+                    Upload of relevant documents
                   </label>
                   <input
                     type='file'
                     className='form-control-file'
-                    id='relevantDocs'
-                    name='relevantDocs'
+                    id='relevantDocuments'
+                    name='relevantDocuments'
+                    required=''
                   />
                 </div>
-                <button type='submit' className='btn btn-primary mt-3 mb-3'>
+                <button type='submit' className='btn btn-primary'>
                   Submit
                 </button>
               </form>
