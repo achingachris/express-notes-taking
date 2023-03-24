35eed94b9b2d6b1ed29870c394553ed49c15ec74
the CodeFather, but with less billions <achinga.chris@gmail.com>
2023-03-24 23:56:14 +0300

    add & view notes

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+
+const app = express()
+const notes = [] // In-memory notes storage
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+// List notes
+app.get('/notes', (req, res) => {
+  res.json(notes)
+})
+
+// Add note
+app.post('/notes', (req, res) => {
+  const { title, content } = req.body
+  const newNote = { id: Date.now(), title, content }
+  notes.push(newNote)
+  res.status(201).json(newNote)
+})
+
+// Edit note
+app.put('/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id)
+  const { title, content } = req.body
+
+  const noteIndex = notes.findIndex((note) => note.id === noteId)
+
+  if (noteIndex === -1) {
+    res.status(404).json({ error: 'Note not found' })
+  } else {
+    notes[noteIndex].title = title
+    notes[noteIndex].content = content
+    res.json(notes[noteIndex])
+  }
+})
+
+// Delete note
+app.delete('/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id)
+  const noteIndex = notes.findIndex((note) => note.id === noteId)
+
+  if (noteIndex === -1) {
+    res.status(404).json({ error: 'Note not found' })
+  } else {
+    notes.splice(noteIndex, 1)
+    res.status(204).end()
+  }
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
