f748fb225fe42e46242fcf006f4074ce65bfa5bd
the CodeFather, but with less billions <achinga.chris@gmail.com>
2023-03-25 00:51:47 +0300

    updated: CRUD features

--- app.js
+++ app.js
@@ -1,61 +1,76 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
-const notes = [] // In-memory notes storage
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.json())
-app.use(express.static('public'))
+const NOTES_FILE = 'notes.json';
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-})
-
-// List notes
-app.get('/notes', (req, res) => {
-  res.json(notes)
-})
-
-// Add note
-app.post('/notes', (req, res) => {
-  const { title, content } = req.body
-  const newNote = { id: Date.now(), title, content }
-  notes.push(newNote)
-  res.status(201).json(newNote)
-})
-
-// Edit note
-app.put('/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    const { title, content } = req.body;
-  
-    // Find the note in the notes array by its ID
-    const noteIndex = notes.findIndex(note => note.id === noteId);
-  
-    if (noteIndex !== -1) {
-      // Update the note
-      notes[noteIndex].title = title;
-      notes[noteIndex].content = content;
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
+function readNotes() {
+    try {
+        return JSON.parse(fs.readFileSync(NOTES_FILE, 'utf8'));
+    } catch (err) {
+        return [];
     }
-  });
-  
+}
 
-// Delete note
+function writeNotes(notes) {
+    fs.writeFileSync(NOTES_FILE, JSON.stringify(notes, null, 2));
+}
+
+app.get('/notes', (req, res) => {
+    res.json(readNotes());
+});
+
+app.post('/notes', (req, res) => {
+    const notes = readNotes();
+    const newNote = { ...req.body, id: uuidv4() };
+    notes.push(newNote);
+    writeNotes(notes);
+    res.status(201).json(newNote);
+});
+
+app.get('/notes/:id', (req, res) => {
+    const note = readNotes().find((note) => note.id === req.params.id);
+    if (note) {
+        res.json(note);
+    } else {
+        res.status(404).json({ error: 'Note not found' });
+    }
+});
+
+app.put('/notes/:id', (req, res) => {
+    const notes = readNotes();
+    const noteIndex = notes.findIndex((note) => note.id === req.params.id);
+    if (noteIndex !== -1) {
+        notes[noteIndex] = { ...notes[noteIndex], ...req.body };
+        writeNotes(notes);
+        res.json(notes[noteIndex]);
+    } else {
+        res.status(404).json({ error: 'Note not found' });
+    }
+});
+
 app.delete('/notes/:id', (req, res) => {
-  const noteId = parseInt(req.params.id)
-  const noteIndex = notes.findIndex((note) => note.id === noteId)
+    const notes = readNotes();
+    const noteIndex = notes.findIndex((note) => note.id === req.params.id);
+    if (noteIndex !== -1) {
+        notes.splice(noteIndex, 1);
+        writeNotes(notes);
+        res.json({ message: 'Note deleted' });
+    } else {
+        res.status(404).json({ error: 'Note not found' });
+    }
+});
 
-  if (noteIndex === -1) {
-    res.status(404).json({ error: 'Note not found' })
-  } else {
-    notes.splice(noteIndex, 1)
-    res.status(204).end()
-  }
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
